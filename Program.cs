7e8a1031b1e8cfccaa85577acd9a7d1212f58020
Mult0 <taezarex@gmail.com>
2025-05-29 18:52:51 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace STUDY.mathgame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? readResult;
+
+            do
+            {
+                Console.WriteLine("MATH MINI GAME !!");
+                Console.WriteLine("CHOOOOOOSEEEE AN OPERATON:");
+                Console.WriteLine("1. MULTIPLICATION");
+                Console.WriteLine("2. DIVISION");
+                Console.WriteLine("3. ADDITION");
+                Console.WriteLine("4. SUBTRACTION");
+            }
+            while (true);
+        }
+    }
+}
