bafbb1ed92447a403d924c95087d103146c3a9c6
Mult0 <taezarex@gmail.com>
2025-05-29 18:58:58 +0800

    Added a switch case for menu selection.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             string? readResult;
+            string menuSelect = "";
 
             do
             {
@@ -14,8 +15,34 @@
                 Console.WriteLine("2. DIVISION");
                 Console.WriteLine("3. ADDITION");
                 Console.WriteLine("4. SUBTRACTION");
+
+                readResult = Console.ReadLine();
+
+                if (readResult != null)
+                {
+                    menuSelect = readResult.ToLower();
+                }
+
+                switch(menuSelect)
+                {
+                    case "1":
+
+                        break;
+                    case "2":
+
+                        break;
+                    case "3":
+
+                        break;
+                    case "4":
+
+                        break;
+                    default:
+
+                        break;
+                }
             }
-            while (true);
+            while (menuSelect != "exit");
         }
     }
 }
