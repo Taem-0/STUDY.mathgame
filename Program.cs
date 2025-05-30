12537b8682a066f8f4fa0d447fc2a526361878d6
Mult0 <taezarex@gmail.com>
2025-05-30 23:34:53 +0800

    Completed game requirements. Journey to publishing a mech game.

--- Program.cs
+++ Program.cs
@@ -1,21 +1,29 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+//Global Variables
 int maxGames = 5;
 string? readResult;
 string menuSelect = "";
+bool validEntry = false;
+int playerAnswer = 0;
+var response = "";
+List<string> gameRecords = new List<string>(); 
 Random numberGenerator = new Random();
 
-MainMenu();
+MainMenu();     
 
 void MainMenu()
 {
     do
-    {
+        {
+
         Console.WriteLine("MATH MINI GAME !!");
         Console.WriteLine("CHOOOOOOSEEEE AN OPERATON:");
         Console.WriteLine("M. MULTIPLICATION");
         Console.WriteLine("D. DIVISION");
         Console.WriteLine("A. ADDITION");
         Console.WriteLine("S. SUBTRACTION");
+        Console.WriteLine("R. RECORDS");
         Console.WriteLine("Q. QUIT");
 
         readResult = Console.ReadLine();
@@ -28,16 +36,26 @@ void MainMenu()
         switch (menuSelect)
         {
             case "m":
-                MultiplicationGame();
+                MultiplicationGame("MULTIPLICATION !! GAME,,,,, START !!");
                 break;
             case "d":
-                DivisionGame();
+                DivisionGame("DIVISION !! GAME,,,,, START !!");
                 break;
             case "a":
                 AdditionGame("ADDITION !! GAME,,,,, START !!");
                 break;
             case "s":
-                SubtractionGame();
+                SubtractionGame("SUBTRACTION !! GAME,,,,, START !!");
+                break;
+            case "r":
+                foreach (string games in gameRecords)
+                {
+                    Console.WriteLine($"{games}\n");
+                }
+
+                Console.ReadLine();
+                Console.Clear();
+
                 break;
             default:
 
@@ -49,47 +67,257 @@ void MainMenu()
 }
 
 
-void MultiplicationGame()
+
+void MultiplicationGame(string message)
 {
-    
-}
+    int correctAnswers = 0;
 
-
-void DivisionGame()
-{
-    
-}
-
-
-void SubtractionGame()
-{
-    
-}
-
-
-void AdditionGame(string message)
-{
     Console.WriteLine($"{message}");
 
     for (int i = 0; i < maxGames; i++)
     {
-        int firstNumber = numberGenerator.Next(1, 99);
-        int secondNumber = numberGenerator.Next(1, 99);
-        int correctAnswer = firstNumber + secondNumber;
+        int firstNumber = numberGenerator.Next(1, 9);
+        int secondNumber = numberGenerator.Next(1, 9);
 
-        Console.WriteLine($@"ANSWER THIS: {firstNumber} + {secondNumber} = ");
 
-        var response = Console.ReadLine();
-
-        if (int.Parse(response) == correctAnswer)
+        do
         {
-            Console.WriteLine($"CORRECT !! {response} IS CORRECT");
-        }
-        else if (int.Parse(response) != correctAnswer)
-        {
-            Console.WriteLine($"WRONG !! {response} IS NOT CORRECT");
-        }
+            Console.WriteLine($@"ANSWER THIS: {firstNumber} x {secondNumber} = ");
+
+
+            readResult = Console.ReadLine();
+            if (readResult != null)
+            {
+                response = readResult.ToLower().Trim();
+                validEntry = int.TryParse(response, out playerAnswer);
+            }
+
+            if (validEntry == true)
+            {
+                if (playerAnswer == firstNumber * secondNumber)
+                {
+                    Console.WriteLine($"CORRECT !! {response} IS CORRECT");
+                    correctAnswers++;
+                    Console.WriteLine();
+                }
+                else if (playerAnswer != firstNumber * secondNumber)
+                {
+                    Console.WriteLine($"WRONG !! {response} IS NOT CORRECT");
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("INPUT A NUMBER !!");
+            }
+
+        } while (validEntry == false);
+
+
+
     }
-        
+
+    gameRecords.Add($"DATE: {DateTime.Now}\t\tMODE: MULTIPLICATION\t\tSCORE: {correctAnswers}");
+    Console.WriteLine($"YOU ANSWERED {correctAnswers} out of {maxGames}\nPRESS ANY BUTTON TO CONTINUE");
+    Console.ReadLine();
+    Console.Clear();    
+}
+
+void DivisionGame(string message) 
+{
+    int correctAnswers = 0;
+
+    Console.WriteLine($"{message}");
+
+    for (int i = 0; i < maxGames; i++)
+    {
+        var divisionNumbers = DivisionGameNumberGenerator();
+        int firstNumber = divisionNumbers[0];
+        int secondNumber = divisionNumbers[1];
+
+
+        do
+        {
+            Console.WriteLine($@"ANSWER THIS: {firstNumber} / {secondNumber} = ");
+
+
+            readResult = Console.ReadLine();
+            if (readResult != null)
+            {
+                response = readResult.ToLower().Trim();
+                validEntry = int.TryParse(response, out playerAnswer);
+            }
+
+            if (validEntry == true)
+            {
+                if (playerAnswer == firstNumber / secondNumber)
+                {
+                    Console.WriteLine($"CORRECT !! {response} IS CORRECT");
+                    correctAnswers++;
+                    Console.WriteLine();
+                }
+                else if (playerAnswer != firstNumber / secondNumber)
+                {
+                    Console.WriteLine($"WRONG !! {response} IS NOT CORRECT");
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("INPUT A NUMBER !!");
+            }
+
+        } while (validEntry == false);
+
+
+
+    }
+
+    gameRecords.Add($"DATE: {DateTime.Now}\t\tMODE: DIVISION\t\tSCORE: {correctAnswers}");
+    Console.WriteLine($"YOU ANSWERED {correctAnswers} out of {maxGames}\nPRESS ANY BUTTON TO CONTINUE");
+    Console.ReadLine();
+    Console.Clear();
+
+
+
+
 
 }
+
+int[] DivisionGameNumberGenerator()
+{
+    Random divisionNumberGenerator = new Random();
+    int firstNumber = numberGenerator.Next(1, 99);
+    int secondNumber = numberGenerator.Next(1, 99);
+
+    int[] result = new int[2];
+
+    while (firstNumber % secondNumber != 0)
+    {
+        firstNumber = numberGenerator.Next(1, 99);
+        secondNumber = numberGenerator.Next(1, 99);
+    }
+
+    result[0] = firstNumber;
+    result[1] = secondNumber;
+
+    return result;
+}
+
+void AdditionGame(string message)
+{
+
+    int correctAnswers = 0;
+
+    Console.WriteLine($"{message}");
+
+    for (int i = 0; i < maxGames; i++)
+    {
+        int firstNumber = numberGenerator.Next(1, 9);
+        int secondNumber = numberGenerator.Next(1, 9);
+
+
+        do
+        {
+            Console.WriteLine($@"ANSWER THIS: {firstNumber} + {secondNumber} = ");
+
+
+            readResult = Console.ReadLine();
+            if (readResult != null)
+            {
+                response = readResult.ToLower().Trim();
+                validEntry = int.TryParse(response, out playerAnswer);
+            }
+
+            if (validEntry == true)
+            {
+                if (playerAnswer == firstNumber + secondNumber)
+                {
+                    Console.WriteLine($"CORRECT !! {response} IS CORRECT");
+                    correctAnswers++;
+                    Console.WriteLine();
+                }
+                else if (playerAnswer != firstNumber + secondNumber)
+                {
+                    Console.WriteLine($"WRONG !! {response} IS NOT CORRECT");
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("INPUT A NUMBER !!");
+            }
+
+        } while (validEntry == false);
+
+
+
+    }
+
+    gameRecords.Add($"DATE: {DateTime.Now}\t\tMODE: ADDITION\t\tSCORE: {correctAnswers}");
+    Console.WriteLine($"YOU ANSWERED {correctAnswers} out of {maxGames}\nPRESS ANY BUTTON TO CONTINUE");
+    Console.ReadLine();
+    Console.Clear();
+
+
+}
+
+void SubtractionGame(string message)
+{
+
+    int correctAnswers = 0;
+
+    Console.WriteLine($"{message}");
+
+    for (int i = 0; i < maxGames; i++)
+    {
+        int firstNumber = numberGenerator.Next(1, 9);
+        int secondNumber = numberGenerator.Next(1, 9);
+
+
+        do
+        {
+            Console.WriteLine($@"ANSWER THIS: {firstNumber} - {secondNumber} = ");
+
+
+            readResult = Console.ReadLine();
+            if (readResult != null)
+            {
+                response = readResult.ToLower().Trim();
+                validEntry = int.TryParse(response, out playerAnswer);
+            }
+
+            if (validEntry == true)
+            {
+                if (playerAnswer == firstNumber - secondNumber)
+                {
+                    Console.WriteLine($"CORRECT !! {response} IS CORRECT");
+                    correctAnswers++;
+                    Console.WriteLine();
+                }
+                else if (playerAnswer != firstNumber - secondNumber)
+                {
+                    Console.WriteLine($"WRONG !! {response} IS NOT CORRECT");
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("INPUT A NUMBER !!");
+            }
+
+        } while (validEntry == false);
+
+
+
+    }
+
+    gameRecords.Add($"DATE: {DateTime.Now}\t\tMODE: SUBTRACTION\t\tSCORE: {correctAnswers}");
+    Console.WriteLine($"YOU ANSWERED {correctAnswers} out of {maxGames}\nPRESS ANY BUTTON TO CONTINUE");
+    Console.ReadLine();
+    Console.Clear();
+}
+
+
+
+
