dc25110c3ad69d711d75d89032b0cda9e85f3943
Mult0 <taezarex@gmail.com>
2025-05-31 02:05:58 +0800

    Implemented a difficulty choice.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 //Global Variables
-int maxGames = 5;
+int maxGames = 0;
+int numberRange = 0;
 string? readResult;
 string menuSelect = "";
 bool validEntry = false;
@@ -23,7 +24,7 @@ void MainMenu()
         Console.WriteLine("D. DIVISION");
         Console.WriteLine("A. ADDITION");
         Console.WriteLine("S. SUBTRACTION");
-        Console.WriteLine("R. RECORDS");
+        Console.WriteLine("H. HISTORY");
         Console.WriteLine("Q. QUIT");
 
         readResult = Console.ReadLine();
@@ -32,6 +33,8 @@ void MainMenu()
         {
             menuSelect = readResult.ToLower().Trim();
         }
+
+        Console.Clear();
 
         switch (menuSelect)
         {
@@ -47,7 +50,9 @@ void MainMenu()
             case "s":
                 SubtractionGame("SUBTRACTION !! GAME,,,,, START !!");
                 break;
-            case "r":
+            case "h":
+                Console.WriteLine("LOOK INTO THEEE,,,,, CRYSTAL BALL OF RECORDS !!");
+
                 foreach (string games in gameRecords)
                 {
                     Console.WriteLine($"{games}\n");
@@ -67,17 +72,19 @@ void MainMenu()
 }
 
 
-
+//GAMES
 void MultiplicationGame(string message)
 {
+    gameDifficulty(maxGames, numberRange);
+
     int correctAnswers = 0;
 
     Console.WriteLine($"{message}");
 
     for (int i = 0; i < maxGames; i++)
     {
-        int firstNumber = numberGenerator.Next(1, 9);
-        int secondNumber = numberGenerator.Next(1, 9);
+        int firstNumber = numberGenerator.Next(1, numberRange);
+        int secondNumber = numberGenerator.Next(1, numberRange);
 
 
         do
@@ -125,6 +132,8 @@ void MultiplicationGame(string message)
 
 void DivisionGame(string message) 
 {
+    gameDifficulty(maxGames, numberRange);
+
     int correctAnswers = 0;
 
     Console.WriteLine($"{message}");
@@ -184,28 +193,9 @@ void DivisionGame(string message)
 
 }
 
-int[] DivisionGameNumberGenerator()
-{
-    Random divisionNumberGenerator = new Random();
-    int firstNumber = numberGenerator.Next(1, 99);
-    int secondNumber = numberGenerator.Next(1, 99);
-
-    int[] result = new int[2];
-
-    while (firstNumber % secondNumber != 0)
-    {
-        firstNumber = numberGenerator.Next(1, 99);
-        secondNumber = numberGenerator.Next(1, 99);
-    }
-
-    result[0] = firstNumber;
-    result[1] = secondNumber;
-
-    return result;
-}
-
 void AdditionGame(string message)
 {
+    gameDifficulty(maxGames, numberRange);
 
     int correctAnswers = 0;
 
@@ -213,8 +203,8 @@ void AdditionGame(string message)
 
     for (int i = 0; i < maxGames; i++)
     {
-        int firstNumber = numberGenerator.Next(1, 9);
-        int secondNumber = numberGenerator.Next(1, 9);
+        int firstNumber = numberGenerator.Next(1, numberRange);
+        int secondNumber = numberGenerator.Next(1, numberRange);
 
 
         do
@@ -264,6 +254,7 @@ void AdditionGame(string message)
 
 void SubtractionGame(string message)
 {
+    gameDifficulty(maxGames, numberRange);
 
     int correctAnswers = 0;
 
@@ -271,8 +262,8 @@ void SubtractionGame(string message)
 
     for (int i = 0; i < maxGames; i++)
     {
-        int firstNumber = numberGenerator.Next(1, 9);
-        int secondNumber = numberGenerator.Next(1, 9);
+        int firstNumber = numberGenerator.Next(1, numberRange);
+        int secondNumber = numberGenerator.Next(1, numberRange);
 
 
         do
@@ -318,6 +309,90 @@ void SubtractionGame(string message)
     Console.Clear();
 }
 
+
+
+int[] gameDifficulty(int levels, int difficultyRange)
+{
+    string difficulty = "";
+
+    int[] results = new int[2];
+  
+    Console.WriteLine("CHOOSE YOUR DIFFICULTY, CHALLENGER !!");
+    Console.WriteLine("ONE, EASY");
+    Console.WriteLine("TWO, MEDIUM");
+    Console.WriteLine("THREE, HARD");
+
+    readResult = Console.ReadLine();
+
+    if (readResult != null)
+    {
+            difficulty = readResult.ToLower().Trim();
+    }
+
+    Console.Clear();
+
+    switch(difficulty)
+    {
+        case "1":
+            Console.WriteLine("YOU CHOSE EASY !!");
+            Console.ReadLine();
+            Console.Clear();
+
+            levels = 5;
+            difficultyRange = 9;
+
+            break;
+        case "2":
+            Console.WriteLine("YOU CHOSE MEDIUM !!");
+            Console.ReadLine();
+            Console.Clear();
+
+
+            levels = 10;
+            difficultyRange = 49;
+
+            break;
+        case "3":
+            Console.WriteLine("YOU CHOSE HARD !!");
+            Console.ReadLine();
+            Console.Clear();
+
+            levels = 20;
+            difficultyRange = 99;
+
+            break;
+        default:
+            Console.WriteLine("INVALID !!");
+
+            break;
+    }
+
+    results[0] = maxGames = levels;
+    results[1] = numberRange = difficultyRange;
+
+    return results; 
+
+}
+
+int[] DivisionGameNumberGenerator()
+{
+    Random divisionNumberGenerator = new Random();
+    int firstNumber = numberGenerator.Next(1, numberRange);
+    int secondNumber = numberGenerator.Next(1, numberRange);
+
+    int[] result = new int[2];
+
+    while (firstNumber % secondNumber != 0)
+    {
+        firstNumber = numberGenerator.Next(1, 99);
+        secondNumber = numberGenerator.Next(1, 99);
+    }
+
+    result[0] = firstNumber;
+    result[1] = secondNumber;
+
+    return result;
+}
 
 
 
